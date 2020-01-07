f222a1b9dce74ccf6456efd54fc674a501d44684
Joe Previte <jjprevite@gmail.com>
2020-01-07 13:27:09 -0700

    feat: add initial mvp

---
+++ pages/api/generateSocialImage.ts
@@ -0,0 +1,116 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface Config {
+  title: string;
+  tagline: string;
+  cloudName: string;
+  imagePublicID: string;
+  cloudinaryUrlBase?: string;
+  titleExtraConfig?: string;
+  taglineExtraConfig?: string;
+  titleFont?: string;
+  taglineFont?: string;
+  imageWidth?: number;
+  imageHeight?: number;
+  textAreaWidth?: number;
+  textLeftOffset?: number;
+  titleBottomOffset?: number;
+  taglineTopOffset?: number;
+  textColor?: string;
+  titleFontSize?: number;
+  taglineFontSize?: number;
+  version?: string;
+}
+
+function generateSocialImage({
+  title,
+  tagline,
+  cloudName = 'dobfxs62e',
+  imagePublicID = 'example-template',
+  cloudinaryUrlBase = 'https://res.cloudinary.com',
+  titleFont = 'arial',
+  titleExtraConfig = '',
+  taglineExtraConfig = '',
+  taglineFont = 'arial',
+  imageWidth = 1280,
+  imageHeight = 669,
+  textAreaWidth = 760,
+  textLeftOffset = 480,
+  titleBottomOffset = 254,
+  taglineTopOffset = 445,
+  textColor = '000000',
+  titleFontSize = 64,
+  taglineFontSize = 48,
+  version = null,
+}: Config): string {
+  // configure social media image dimensions, quality, and format
+  const imageConfig = [
+    `w_${imageWidth}`,
+    `h_${imageHeight}`,
+    'c_fill',
+    'q_auto',
+    'f_auto',
+  ].join(',');
+
+  // configure the title text
+  const titleConfig = [
+    `w_${textAreaWidth}`,
+    'c_fit',
+    `co_rgb:${textColor}`,
+    'g_south_west',
+    `x_${textLeftOffset}`,
+    `y_${titleBottomOffset}`,
+    `l_text:${titleFont}_${titleFontSize}${titleExtraConfig}:${encodeURIComponent(
+      title
+    )}`,
+  ].join(',');
+
+  // configure the tagline text
+  const taglineConfig = [
+    `w_${textAreaWidth}`,
+    'c_fit',
+    `co_rgb:${textColor}`,
+    'g_north_west',
+    `x_${textLeftOffset}`,
+    `y_${taglineTopOffset}`,
+    `l_text:${taglineFont}_${taglineFontSize}${taglineExtraConfig}:${encodeURIComponent(
+      tagline
+    )}`,
+  ].join(',');
+
+  // combine all the pieces required to generate a Cloudinary URL
+  const urlParts = [
+    cloudinaryUrlBase,
+    cloudName,
+    'image',
+    'upload',
+    imageConfig,
+    titleConfig,
+    taglineConfig,
+    version,
+    imagePublicID,
+  ];
+
+  // remove any falsy sections of the URL (e.g. an undefined version)
+  const validParts = urlParts.filter(Boolean);
+
+  // join all the parts into a valid URL to the generated image
+  return validParts.join('/');
+}
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.body) {
+    const { title, tagline } = req.body;
+    const testImage = generateSocialImage({
+      title,
+      tagline,
+      cloudName: 'dobfxs62e',
+      imagePublicID: 'example-template',
+    });
+    // Write our code here.
+    res.status(200).json({ url: testImage });
+  } else {
+    res.status(404).json({ message: 'Missing body' });
+  }
+  // res.status(200).json({ title: 'Next.js' });
+};
---
+++ pages/index.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => {
+
+  const [title, setTitle] = React.useState('')
+  const [tagline, setTagline] = React.useState('')
+  const [generatedImage, setGeneratedImage] = React.useState('')
+
+  return (
+    <div className='main-app'>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <div className="hero">
+        <h1 className="title">Welcome to Next.js!</h1>
+        <p className="description">
+          To get started, edit <code>pages/index.js</code> and save to reload.
+      </p>
+
+        <div className="row">
+          {generatedImage &&
+            <img src={generatedImage} alt="generated social image." />
+          }
+          <form onSubmit={async (e) => {
+            e.preventDefault()
+            try {
+              // make call to api
+              const response = await fetch('/api/generateSocialImage', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title, tagline })
+              })
+
+              const data = await response.json();
+              // set image url
+              setGeneratedImage(data.url)
+
+            } catch (error) {
+              console.error(error, 'oops')
+
+            }
+
+          }}>
+            <input type="text" placeholder="hello" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" placeholder="hello" value={tagline} onChange={(e) => setTagline(e.target.value)} />
+            <button type="submit">Generate image</button>
+          </form>
+        </div>
+      </div>
+
+      <style jsx>{`
+      .main-app {
+        background-color: black;
+        height: 100vh;
+      }
+      .hero {
+        width: 100%;
+        color: #fff;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+      .card {
+        padding: 18px 18px 24px;
+        width: 220px;
+        text-align: left;
+        text-decoration: none;
+        color: #434343;
+        border: 1px solid #9b9b9b;
+      }
+      .card:hover {
+        border-color: #067df7;
+      }
+      .card h3 {
+        margin: 0;
+        color: #067df7;
+        font-size: 18px;
+      }
+      .card p {
+        margin: 0;
+        padding: 12px 0 0;
+        font-size: 13px;
+        color: #333;
+      }
+    `}</style>
+    </div>
+  )
+}
+
+export default Home
